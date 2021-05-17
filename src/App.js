f10146c7ceb2b2e2008674b6237feb49a285a07f
Marina Faraji <marina.faraji@bbc.co.uk>
2021-05-17 20:57:08 +0100

    Add router and config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import ReadingList from './components/ReadingList';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddBook from './components/AddBook';
 
 function App() {
   return (
     <div className="App">
-      <ReadingList />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/reading-list" component={ReadingList} />
+          <Route path="/add-book" component={AddBook} />
+        </Switch>
+      </div>
+    </Router>    </div>
   );
 }
 
