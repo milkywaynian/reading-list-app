7eaa71242500856a51005a62559ed335eb35a35d
Marina Faraji <marina.faraji@bbc.co.uk>
2021-05-21 07:59:30 +0100

    convert a function to a class

--- src/components/ReadingList.js
+++ src/components/ReadingList.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Button } from 'react-bootstrap'; 
 
-function ReadingList() {
-    return (
-        <div>
+// function ReadingList() {
+//     return (
+//         <div>
+//             <h1>Reading List</h1>
+//             <Button variant="primary" href='/add-book'>Add Book</Button>
+//             <table>
+//                 <tr>
+//                     <th>Title</th>
+//                     <th>Author</th>
+//                     <th>Read</th>
+//                 </tr>
+//             </table>
+//         </div>
+//     )
+
+// }
+
+class ReadingList extends React.Component {
+    render() {
+        return (
+            <div>
             <h1>Reading List</h1>
             <Button variant="primary" href='/add-book'>Add Book</Button>
             <table>
@@ -14,8 +32,9 @@ function ReadingList() {
                 </tr>
             </table>
         </div>
-    )
-
+        )
+    }
 }
+
 
 export default ReadingList;
